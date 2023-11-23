795e6652142292a49d2ae1cee993a7f3bfe2590c
rikiariady <150464586+rikiariady@users.noreply.github.com>
2023-11-23 16:00:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// Tambahkan ini di dalam skrip untuk memberikan nama pada video
-document.addEventListener('DOMContentLoaded', function() {
-    // Mengambil elemen video
-    const video = document.getElementById('videoPlayer');
-    
-    // Memberikan nama pada video
-    const videoName = 'Vidio Pertama';
-    video.setAttribute('title', videoName);
-    
-    // Output nama video ke konsol (opsional)
-    console.log('Nama Video:', videoName);
-});￼Enter
+document.getElementById("menu-icon").addEventListener("click", function() {
+    var nav = document.getElementById("nav");
+    nav.style.display = (nav.style.display === "block") ? "none" : "block";
+})
