7a33c74d7c00e3fcb477500c4fcc74c0f53db428
rikiariady <150464586+rikiariady@users.noreply.github.com>
2023-11-24 14:17:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const hamburger = document.getElementById('hamburger');
 const nav = document.querySelector('nav');
 
 hamburger.addEventListener('click', function () {
+    console.log('Hamburger clicked!');
     nav.classList.toggle('active');
 });
